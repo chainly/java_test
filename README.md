a76b37f0ecfbdfbc8658adb210b5254faf087ecd
chainly <1258626769@qq.com>
2020-08-16 05:44:57 +0800

    add read in

--- README.md
+++ README.md
@@ -10,6 +10,15 @@ A: https://stackoverflow.com/a/45278119/6493535 ==> **local repository location 
 
 ## main
 
+Q: no promote but only for x.
+A: Window ！！> Preferences ！！> Java ！！> Editor ！！> Content Assist, then add `.abcdefghijklmnopqrstuvwxyz` to `auto activation triggers for java`
+
 ## io
 
-### 
+### read in
+
+Q: Unhandled exception type IOException
+A: 1. add `try catch` to handle IOException
+2. or add `throws IOException` to let called to handle it
+
+### print out
--- src/com/opqian/test/MainTest.java
+++ src/com/opqian/test/MainTest.java
@@ -1,8 +1,21 @@
 package com.opqian.test;
 
-public class MainTest {
-	public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class MainTest{
+	public static void main(String[] args) throws IOException {
 		System.out.println("main");
+		InputStreamReader bis = new InputStreamReader(System.in);
+		BufferedReader in = new BufferedReader(bis);
+		char c;
+		int n;
+		n = in.read(); // 97
+		c = (char) n; // a
+		System.out.println(c);
+		System.out.println(n);
+
 	}
 
 }
